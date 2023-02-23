80d12e47115b1b375c2924f3473e657a092d24ba
codeBerzerk <alphatech664@gmail.com>
2023-02-23 09:58:22 +0200

    update AboutUs block

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -16,7 +16,7 @@ export default class AboutUs extends React.Component {
         const { t } = this.props;
         return (
             <div className="aboutUS">
-                <div className="aboutUS__animation"></div>
+                <div className="aboutUS__main-animation"></div>
             <Logo/>
                 <button className="aboutUS__button">{t("order_a_service")}</button>
                 <Dots/>
@@ -25,6 +25,8 @@ export default class AboutUs extends React.Component {
                     <div className="aboutUS__text-title">{t("aboutUs__text_title")}</div>
                     <div className="aboutUS__text-description">{t("aboutUs__text_description")}</div>
                 </div>
+                <div className="aboutUS__ball-animation"></div>
+                <Dots/>
             </div>
         );
     }
