bcbd166a7b4a19a7f6b9cb1ae7adeb6a4c2a9262
codeBerzerk <alphatech664@gmail.com>
2023-02-24 00:18:40 +0200

    fixes

--- src/components/OurApproach/OurApproach.jsx
+++ src/components/OurApproach/OurApproach.jsx
@@ -15,13 +15,47 @@ export default class OurApproach extends React.Component {
                 <div className="our-approach__title">{t("our_approach__title")}</div>
                 <div className="our-approach__ball-animation"></div>
                 <div className="our-approach__step-container">
-
+                    <span>
                     <div className="our-approach__step-container__title">
                         ПРОФЕСІЙНЕ ВИВЧЕННЯ УСІХ ТОНКОСТЕЙ ВАШОГО БІЗНЕСУ.
                     </div>
                     <DotsLarge/>
+                    </span>
                     <div className="our-approach__step-container__description">
-
+                        Саме так, ми не просто беремося і робимо що в голову влізе, ми аналізуємо Ваш товар чи послугу, Ваших конкурентів, та Ваших клієнтів, задля того щоб розробити сайт який буде відрізнятися від інших сайтів конкурентів  своєю оригінальністю та практичністю.
+                    </div>
+                </div>
+                <div className="our-approach__step-container">
+                    <span>
+                    <div className="our-approach__step-container__title">
+                        ПРОФЕСІЙНЕ ВИВЧЕННЯ УСІХ ТОНКОСТЕЙ ВАШОГО БІЗНЕСУ.
+                    </div>
+                    <DotsLarge/>
+                    </span>
+                    <div className="our-approach__step-container__description">
+                        Саме так, ми не просто беремося і робимо що в голову влізе, ми аналізуємо Ваш товар чи послугу, Ваших конкурентів, та Ваших клієнтів, задля того щоб розробити сайт який буде відрізнятися від інших сайтів конкурентів  своєю оригінальністю та практичністю.
+                    </div>
+                </div>
+                <div className="our-approach__step-container">
+                    <span>
+                    <div className="our-approach__step-container__title">
+                        ПРОФЕСІЙНЕ ВИВЧЕННЯ УСІХ ТОНКОСТЕЙ ВАШОГО БІЗНЕСУ.
+                    </div>
+                    <DotsLarge/>
+                    </span>
+                    <div className="our-approach__step-container__description">
+                        Саме так, ми не просто беремося і робимо що в голову влізе, ми аналізуємо Ваш товар чи послугу, Ваших конкурентів, та Ваших клієнтів, задля того щоб розробити сайт який буде відрізнятися від інших сайтів конкурентів  своєю оригінальністю та практичністю.
+                    </div>
+                </div>
+                <div className="our-approach__step-container">
+                    <span>
+                    <div className="our-approach__step-container__title">
+                        ПРОФЕСІЙНЕ ВИВЧЕННЯ УСІХ ТОНКОСТЕЙ ВАШОГО БІЗНЕСУ.
+                    </div>
+                    <DotsLarge/>
+                    </span>
+                    <div className="our-approach__step-container__description">
+                        Саме так, ми не просто беремося і робимо що в голову влізе, ми аналізуємо Ваш товар чи послугу, Ваших конкурентів, та Ваших клієнтів, задля того щоб розробити сайт який буде відрізнятися від інших сайтів конкурентів  своєю оригінальністю та практичністю.
                     </div>
                 </div>
             </div>
--- src/components/images/DotsLarge.jsx
+++ src/components/images/DotsLarge.jsx
@@ -1,6 +1,6 @@
 function DotsLarge() {
     return (
-        <svg className="our-approach__dots" width="20" height="89" viewBox="0 0 20 89" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg className="our-approach__step-container__dots" width="20" height="90" viewBox="0 0 20 89" fill="none" xmlns="http://www.w3.org/2000/svg">
             <ellipse cx="9.52204" cy="9.52191" rx="9.52191" ry="9.52191" transform="rotate(90 9.52204 9.52191)" fill="#FF9900"/>
             <circle cx="9.52284" cy="38.0873" r="7.93492" transform="rotate(90 9.52284 38.0873)" fill="#FF9900" fillOpacity="0.9"/>
             <circle cx="9.52194" cy="63.4798" r="6.34794" transform="rotate(90 9.52194 63.4798)" fill="#FF9900" fillOpacity="0.8"/>
