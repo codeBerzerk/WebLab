648861b854bf9a6f1481ad36d5bce9eca7a0a250
codeBerzerk <alphatech664@gmail.com>
2023-02-24 15:48:02 +0200

    our Approach not finished

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -18,7 +18,7 @@ export default class AboutUs extends React.Component {
             <div className="aboutUS">
                 <div className="aboutUS__main-animation"></div>
             <Logo/>
-                <button className="aboutUS__button" id="aboutUs">{t("order_a_service")}</button>
+                <button className="aboutUS__button" id="aboutUs"><p className="TEST">{t("order_a_service")}</p></button>
                 <Dots/>
                 <Line/>
                 <div className="aboutUS__text">
