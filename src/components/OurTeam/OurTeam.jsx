af15495cad47c4ebc6c844ade59caf7190241c2e
codeBerzerk <alphatech664@gmail.com>
2023-03-02 00:57:53 +0200

    so many fucking fixes, added some files

--- src/components/OurTeam/OurTeam.jsx
+++ src/components/OurTeam/OurTeam.jsx
@@ -5,6 +5,8 @@ import Line_2 from "../images/Sprites/Line_2";
 import Bohdan from "./Founders/Bohdan/Bohdan";
 import Yaroslav from "./Founders/Yaroslav/Yaroslav";
 import Maria from "./Founders/Maria/Maria";
+import Dots from "../images/Sprites/Dots";
+
 export default class OurTeam extends React.Component {
     state = {
         lang: "ua"
@@ -13,13 +15,20 @@ export default class OurTeam extends React.Component {
     render() {
         const {t} = this.props;
         return (
+            <>
+            <div className="our-team__title">Про нас</div>
             <div className="our-team">
+
                 <Line_2/>
+                <div className="our-team__founders">
                 <Yaroslav/>
                 <Bohdan/>
                 <Maria/>
+                </div>
+                <div className="our-team__dots"><Dots/></div>
                 <div className="our-team__animation"></div>
             </div>
+            </>
         );
 
     }
